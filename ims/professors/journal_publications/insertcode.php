61d39b17818ac91625c9f5ddee0d9424088b6372
Aditya Dhumal <adityadhumal450@gmail.com>
2023-06-15 17:05:26 +0530

    Completed from myside
    Journal Publication
    fdp sttp attended
    conference publications
    completed

--- ims/professors/journal_publications/insertcode.php
+++ ims/professors/journal_publications/insertcode.php
@@ -8,13 +8,14 @@ $queryresult = mysqli_num_rows($query);
     if($queryresult > 0){
         while($row = mysqli_fetch_assoc($query)){ 
             $id = $row['id'];
+            $branch = $row['branch'];
         }  
     }
     
     if(isset($_POST['insertdata']))
     {
         $Name_Of_Faculty = $_POST['Name_Of_Faculty'];
-        $Branch = $_POST['Branch'];
+        // $Branch = $_POST['Branch'];
         $Title_Of_Paper= $_POST['Title_Of_Paper'];
         $Name_Of_Author = $_POST['Name_Of_Author'];
         $Name_Of_Journal = $_POST['Name_Of_Journal'];
@@ -30,13 +31,13 @@ $queryresult = mysqli_num_rows($query);
         $Details_Of_Paper = $_POST['Details_Of_Paper'];
         $Journal_Paper = $_FILES['Journal_Paper']['name'];
         $file_tmp = $_FILES['Journal_Paper']['tmp_name'];
-        $user_id = $_POST['id'];
+        $user_id = $_POST['user_id'];
  
         move_uploaded_file($file_tmp,"JournalPaper/$Journal_Paper");
 
         $query = "INSERT INTO journal_publications
         (`Name_Of_Faculty`, `Branch`, `Title_Of_Paper`, `Name_Of_Author`, `Name_Of_Journal`, `Journal_Citation_Index`, `Journal_Impact_Factor`, `Publisher`, `Year_Of_Publication`, `Publication_Date`, `Volume_Issue`, `ISSN_ISBN`, `Paper_Weblink`, `Link_Of_Paper`, `Details_Of_Paper`, `Journal_Paper`, `user_id`,`STATUS`) 
-        VALUES ('$Name_Of_Faculty', '$Branch', '$Title_Of_Paper', '$Name_Of_Author', '$Name_Of_Journal', '$Journal_Citation_Index', '$Journal_Impact_Factor', '$Publisher', '$Year_Of_Publication', '$Publication_Date', '$Volume_Issue', '$ISSN_ISBN', '$Paper_Weblink', '$Link_Of_Paper', '$Details_Of_Paper', '$Journal_Paper', '$id','PENDING')";
+        VALUES ('$Name_Of_Faculty', '$branch', '$Title_Of_Paper', '$Name_Of_Author', '$Name_Of_Journal', '$Journal_Citation_Index', '$Journal_Impact_Factor', '$Publisher', '$Year_Of_Publication', '$Publication_Date', '$Volume_Issue', '$ISSN_ISBN', '$Paper_Weblink', '$Link_Of_Paper', '$Details_Of_Paper', '$Journal_Paper', '$id','PENDING')";
         
     
         $query_run = mysqli_query($connection, $query);
